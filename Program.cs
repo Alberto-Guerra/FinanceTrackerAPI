a3890c9211b2e95877c7b680ea833bf08365c364
Alberto-Guerra <albertoguerrarodas@gmail.com>
2023-08-22 10:41:06 +0200

    cors enabled

--- Program.cs
+++ Program.cs
@@ -12,7 +12,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>();
-builder.Services.AddCors();
+builder.Services.AddCors(options => options.AddPolicy(name: "TransactionOrigins",
+    policy =>
+    {
+        policy.WithOrigins("financetracker.up.railway.app").AllowAnyHeader().AllowAnyMethod();
+    }
+    ));
+
+
 
 
 var app = builder.Build();
